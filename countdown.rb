35536672e23500d34456274e9f28d49a3bbcba73
Carter Parrett <cparrett94@hotmail.com>
2020-07-08 00:32:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown(number)
+  until number == 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
